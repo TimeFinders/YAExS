8ecc4d80ee15b39a149479cd090b7a6b726bcf56
Andrew Karnani <keeperguy64@gmail.com>
2012-10-31 13:34:33 -0400

    Exam status is now stored via AJAX.
    Still need to update code to use CSRF through jquery.

--- YAExS/Exams/views.py
+++ YAExS/Exams/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, Http404
+from courses.models import Course
 
 
 @login_required
+@csrf_exempt
 def updateExam(request, status=None):
     if request.method == 'POST':
-        pass
-
-    return HttpResponse(status)
-
+        id = request.POST.get('id', None)
+        if id:
+            a = Course.objects.get(id=id)
+            a.status = status
+            a.save()
+        else:
+            raise Http404()
+    return HttpResponse()
