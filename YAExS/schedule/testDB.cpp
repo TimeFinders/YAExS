6d0dad2ba2dc012949970af40c0bcfe7e1fb29b3
Auston Sterling <austonst@gmail.com>
2012-11-05 17:26:01 -0500

    Modified testDB.cpp to properly connect and query.
    This is sort of what we'll need for finding the list of exams, though we'll want to check for cross listed courses and format it in Exams.

--- YAExS/schedule/testDB.cpp
+++ YAExS/schedule/testDB.cpp
@@ -10,10 +10,17 @@
 int main(int argc, char* argv[])
 {
   //Create a DBReader
-  DBReader dbReader;
+  DBReader dbReader("user=andrew password=andrew host=ec2-23-20-128-235.compute-1.amazonaws.com dbname=django port=5432");
 
-  //Try to run "SELECT 1" on the nonexistent database
-  pqxx::result result = dbReader.execute("SELECT 1");
+  //Try to run "SELECT 1" on the database
+  pqxx::result result = dbReader.execute("SELECT * FROM courses_course WHERE status = 'hasexam'");
 
-  std::cout << result[0][0].c_str() << std::endl;
+  for (size_t i = 0; i < result.size(); i++)
+    {
+      for (size_t j = 0; j < result.columns(); j++)
+        {
+          std::cout << result[i][j].c_str() << " ";
+        }
+      std::cout << std::endl;
+    }
 }
