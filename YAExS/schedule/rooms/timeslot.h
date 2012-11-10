b9793448e6df6fc7d4de90ed2d3c7b07668d92e6
Vera Axelrod <vera.axelrod@gmail.com>
2012-11-10 00:38:14 -0500

    Actually added timeslot.h

---
+++ YAExS/schedule/rooms/timeslot.h
@@ -0,0 +1,40 @@
+/*
+  ----- Time Slot Class Declaration-----
+  A class that represents a time slot.
+  This class is just a placeholder that will
+  be improved/expanded later.
+*/
+
+#ifndef _timeslot_h_
+#define _timeslot_h_
+
+
+class TimeSlot
+{
+	private:
+		int id; // e.g. 1..4 on day 1, 5...8 on day 2, etc.
+		
+		
+	public:
+		TimeSlot() : id(-1) {};
+
+		TimeSlot(int id) : id(id) {};
+
+		// compare by smaller (earlier) id
+		bool operator< (const TimeSlot & other){ return this->id < other.id; }; 
+
+		// compare by id
+		bool operator== (const TimeSlot & other){ return this->id == other.id; }; 
+
+		// compare by id
+		bool operator!= (const TimeSlot & other){ return this->id != other.id; }; 
+
+		// compare by id
+		bool operator> (const TimeSlot & other){ return this->id > other.id; }; 
+
+
+		// for debugging
+		int toPrint() { return id; }
+		
+};
+#endif
