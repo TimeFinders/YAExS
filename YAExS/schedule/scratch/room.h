66b1c5caeeb140c9ea87d4afeaf4f1887ae4a623
Vera Axelrod <vera.axelrod@gmail.com>
2012-11-11 22:44:06 -0500

    added more files

---
+++ YAExS/schedule/scratch/room.h
@@ -0,0 +1,78 @@
+/*
+  ----- Room Class Declaration-----
+  A class that represents a room on 
+  campus that can hold exams. This is considered
+  the most basic form of ExamLocation. Other
+  ExamLocations can be groupings of rooms.
+*/
+
+#ifndef _room_h_
+#define _room_h_
+
+#include <string>
+#include "exam.h"
+#include "examlocation.h"
+
+// for debugging
+#include <iostream>
+
+class Room : public ExamLocation
+{
+	friend class RoomGroup;
+
+	private:	
+		// e.g AmosEaton
+		std::string building;
+
+		// e.g. 214
+		 std::string roomAddress;
+
+		// half the number of total seats, 
+		// e.g. AmosEaton 214 has 162 seats so capacity is 81
+		int capacity;
+
+	protected:
+		int getCapacity() const { return capacity; };
+
+	public:
+		//bool isOverlapping(ExamLocation * e) {return false;};
+
+		Room() : building(""), roomAddress(""), capacity(0) {};
+
+		// e.g. new Room("AmosEaton", "214", 162) 
+		Room(std::string building, std::string roomAddress, int totalSeats) : 
+			building(building), 
+			roomAddress(roomAddress), 
+			capacity(totalSeats / 2) {};
+
+		// Return true if the given exam can fit in this room, false otherwise.
+		bool willExamFit(Exam exam) const { return this->capacity >= exam.size(); };
+
+		std::vector<Room> contains() const { return std::vector<Room>(1,*this); }; 
+
+		// for debugging
+		void print() const { std::cout << building << " " << roomAddress;
+			std::cout << " " << " capacity: " << capacity << std::endl; };	
+
+		bool operator== (const Room & r) 
+		{	
+			return (r.building == this-> building && r.roomAddress == this->roomAddress);
+		}
+
+		bool static overlaps(const ExamLocation * e1, const ExamLocation * e2) 
+		{
+			std::vector<Room> rooms1 = e1->contains();
+			std::vector<Room> rooms2 = e2->contains();
+			for(unsigned i = 0; i < rooms1.size(); i++)
+			{
+				for (unsigned j = 0; j < rooms2.size(); j++)
+				{
+					if (rooms1[i] == rooms2[j])
+						return true;
+				}
+			}
+			return false;
+		};
+
+};
+#endif
---
+++ YAExS/schedule/scratch/roomgroup.h
@@ -0,0 +1,50 @@
+/*
+  ----- Room Group Class Declaration-----
+  A class that represents a group of rooms on 
+  campus that can hold a single exam together.
+*/
+
+#ifndef _roomgroup_h_
+#define _roomgroup_h_
+
+#include <string>
+#include <vector>
+#include "exam.h"
+#include "room.h"
+
+// for debugging
+#include <iostream>
+
+class RoomGroup : public ExamLocation
+{
+	private:	
+		// e.g [AmosEaton-214, AmosEaton-215, AmosEaton-216]
+		std::vector<Room> rooms;
+
+		// half the number of total seats in all constituent rooms
+		int capacity;
+
+	protected:
+		int getCapacity() const { return capacity; };
+
+	public:
+		RoomGroup() : rooms(), capacity(0) {};
+
+		RoomGroup(std::vector<Room> constituentRooms) : rooms(constituentRooms)
+		{
+			int totalSize = 0;			
+			for (unsigned i = 0; i < rooms.size(); i++)
+				totalSize += rooms[i].capacity;
+			capacity = totalSize;		
+		 };	
+
+		std::vector<Room> contains() const {return rooms;};	
+
+		// Return true if the given exam can fit in this room group, false otherwise.
+		bool willExamFit(Exam exam) const { return this->capacity >= exam.size(); };
+
+		// for debugging
+		void print() const
+		{ std::cout << "RoomGroup with Total capacity: " << capacity << std::endl; };	
+};
+#endif
