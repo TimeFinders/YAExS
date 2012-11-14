e42557cc55aa745516e49110e0f228cb55fe973d
Vera Axelrod <vera.axelrod@gmail.com>
2012-11-13 21:05:17 -0500

    changed tryAgain/ to rooms/
    moved some things up one directory

--- YAExS/schedule/testOpt.cpp
+++ YAExS/schedule/testOpt.cpp
@@ -4,27 +4,52 @@
 
 #include "optimizer.h"
 #include <iostream>
+#include <vector>
+#include "getsome.h"
+#include <algorithm>
+
+static bool deleteAll( Person * p ) { delete p; return true; }
 
 int main(int argc, char* argv[])
 {
-    //Takes in one argument, the ZIMPL model
-    if (argc != 2)
+
+    // Set up a test example
+
+    std::vector<Person* > people = GetSome::getSomePeople();
+    
+    for (int i = 0; i < people.size(); i++)
     {
-        std::cerr << "Usage: " << argv[0] << " ZIMPL-filename" << std::endl;
-        return 1;
+        std::cout << people[i]->getId() << "'s exams:" <<std::endl;
+        std::vector<Exam> e = people[i]->getExams();
+        for(int j = 0; j < e.size(); j++)
+        {
+            std::cout << '\t' << e[j].getId() << std::endl;
+        }
     }
+    
+    std::vector<Exam> exams = GetSome::getSomeExams();
+    
+    int numDays = 2;
+    int slotsPerDay = 2;
+
 
     //Create an Optimizer
-    Optimizer scipScheduler;
-
-    std::string solution;
-    //Load the ZIMPL model
-    scipScheduler.loadZIMPLModel(argv[1]);
+    Optimizer scipScheduler(true);
+        //Load a model
+    scipScheduler.loadModel(exams, people, numDays, slotsPerDay);
     
     //Get the best solution
     scipScheduler.schedule();
-    solution = scipScheduler.getBestSolution();
+    
+    scipScheduler.printSolutionAndNonzeroValues();
+	
+    // print just the exam schedule
+    std::cout << "\n_________________________\n" << std::endl;
+    scipScheduler.printExamSchedule();
+    std::cout << "\n_________________________\n" << std::endl;
 
-    //Print the solution to cout
-    std::cout << solution << std::endl;
+    std::remove_if( people.begin(), people.end(), deleteAll );
+	
+
+    return 0;
 }
