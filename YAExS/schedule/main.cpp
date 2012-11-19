411ea65af3bf7a3991ca720ae3510ea009f592d7
Auston Sterling <austonst@gmail.com>
2012-11-18 20:15:08 -0500

    Added a real main file and configuration.

---
+++ YAExS/schedule/main.cpp
@@ -0,0 +1,144 @@
+/*
+  -----YAExS Main Program-----
+*/
+
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <unistd.h>
+
+#include "scheduler.h"
+
+int main(int argc, char* argv[])
+{
+        //Check arguments
+        if (argc != 5)
+        {
+                std::cerr << "Usage: " << argv[0] << "days slots-per-day registrations-file pid-file" << std::endl;
+                return 1;
+        }
+
+        //Parse inputs
+        int examDays = atoi(argv[1]);
+        int slotsPerDay = atoi(argv[2]);
+
+        //Load configuration file
+        std::map<std::string,std::string> settings;
+        std::ifstream config("config.txt");
+        if (!config)
+        {
+                std::cerr << "Error opening configuration file config.txt" << std::endl;
+                return 1;
+        }
+
+        //Read the file
+        while (!config.eof())
+        {
+                //Get the next line
+                std::string line;
+                config >> line;
+
+                //Remove all spaces
+                size_t space;
+                while ((space = line.find(' ')) != line.npos) line.erase(space,1);
+
+                //Skip comments
+                if (line[0] == '#') continue;
+
+                //Parse
+                std::string set, value;
+                size_t equals = line.find('=');
+                settings[line.substr(0, equals)] = line.substr(equals+1, line.size()-1);
+        }
+        config.close();
+
+        //Make sure some essential settings are defined
+        if (settings.find("db_username") == settings.end())
+        {
+                std::cerr << "db_username must be defined." << std::endl;
+                return 1;
+        }
+        if (settings.find("db_password") == settings.end())
+        {
+                std::cerr << "db_password must be defined." << std::endl;
+                return 1;
+        }
+        if (settings.find("db_host") == settings.end())
+        {
+                std::cerr << "db_host must be defined." << std::endl;
+                return 1;
+        }
+        if (settings.find("db_name") == settings.end())
+        {
+                std::cerr << "db_name must be defined." << std::endl;
+                return 1;
+        }
+        if (settings.find("db_port") == settings.end())
+        {
+                std::cerr << "db_port must be defined." << std::endl;
+                return 1;
+        }
+        if (settings.find("roomfile") == settings.end())
+        {
+                std::cerr << "roomfile must be defined." << std::endl;
+                return 1;
+        }
+        if (settings.find("roomgroupfile") == settings.end())
+        {
+                std::cerr << "roomgroupfile must be defined." << std::endl;
+                return 1;
+        }
+        
+        //Create a DBManager and Optimizer
+        std::string dbSetupString = "user=" + settings["db_username"];
+        dbSetupString += " password=" + settings["db_password"];
+        dbSetupString += " host=" + settings["db_host"];
+        dbSetupString += " name=" + settings["db_name"];
+        dbSetupString += " port=" + settings["db_port"];
+        DBManager dbManager(dbSetupString);
+        Optimizer opt(false);
+        
+        //Create a Scheduler
+        Scheduler sched(&dbManager, &opt);
+
+        //Try to load exams and students
+        sched.loadExams();
+        if (!sched.loadStudents(argv[3]))
+        {
+                std::cerr << "Could not read " << argv[3] << std::endl;
+                return 1;
+        }
+        
+        //Fork to let the parent return
+        pid_t pid = fork();
+        if (pid < 0) //Error
+        {
+                std::cerr << "Error forking." << std::endl;
+                return 1;
+        }
+        else if (pid > 0) //Parent
+        {
+                //Parent needs to create a pid file before returning
+                std::ofstream pidfile(argv[4], std::ios::out | std::ios::trunc);
+                pidfile << pid;
+                pidfile.close();
+        }
+        else //Child
+        {
+                //Run the scheduler
+                sched.startScheduling(examDays, slotsPerDay);
+
+                //Load locations and assign rooms
+                sched.loadLocations(settings["roomfile"], settings["roomgroupfile"]);
+                sched.assignRooms();
+
+                //Load results into database
+                //sched.writeScheduleToDB();
+
+                //Delete the pid file to let the website know it's done
+                remove(argv[4]);
+        }
+
+        //Finish up
+        return 0;
+}
